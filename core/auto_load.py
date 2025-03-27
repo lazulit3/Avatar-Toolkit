d7fee2c961496977a47a5cbfa8885cced7d9215f
Yusarina <hello@yusarina.xyz>
2025-03-27 20:42:43 +0000

    I don't need to add that check duh

--- core/auto_load.py
+++ core/auto_load.py
@@ -138,15 +138,9 @@ def iter_deps_from_parent_id(cls: Type, my_classes_by_idname: Dict[str, Type]) -
 
 def get_dependency_from_annotation(value: Any) -> Optional[Type]:
     """Get dependency type from a type annotation"""
-    blender_version = bpy.app.version
-    
-    if blender_version >= (2, 93):
-        if isinstance(value, bpy.props._PropertyDeferred):
-            return value.keywords.get("type")
-    else:
-        if isinstance(value, tuple) and len(value) == 2:
-            if value[0] in (bpy.props.PointerProperty, bpy.props.CollectionProperty):
-                return value[1]["type"]
+    if isinstance(value, tuple) and len(value) == 2:
+        if value[0] in (bpy.props.PointerProperty, bpy.props.CollectionProperty):
+            return value[1]["type"]
     return None
 
 def iter_classes_to_register(modules: List[Any]) -> Generator[Type, None, None]:
